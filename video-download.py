7943a3af3c92256efee7c11f3d30cbb52504dd06
Marcio Pancheri <mjpancheri@gmail.com>
2023-02-01 18:46:58 -0300

    change video param from url to id

--- video-download.py
+++ video-download.py
@@ -6,12 +6,12 @@ url = ""
 resolution = "720p"
 
 if len(sys.argv) > 1:
-  url = sys.argv[1]
+  url = "https://www.youtube.com/watch?v=" + sys.argv[1]
 
 if len(sys.argv) > 2:
   resolution = sys.argv[2]
 
 if not url:
-  print("USAGE:\n./video-download VIDEO_URL [RESOLUTION (default 720p)]")
+  print("USAGE:\n./video-download VIDEO_ID [RESOLUTION (default 720p)]")
 else:
   YouTube(url).streams.get_by_resolution(resolution).download()
