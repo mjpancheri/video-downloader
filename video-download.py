a1f9efffe84895529fac91210f8d4481ac7b0d30
Marcio Pancheri <mjpancheri@gmail.com>
2023-02-07 09:13:33 -0300

    feat: improve video-download to get a list of ids

--- video-download.py
+++ video-download.py
@@ -2,16 +2,18 @@ import sys
 
 from pytube import YouTube
 
-url = ""
+ids = ""
 resolution = "720p"
 
 if len(sys.argv) > 1:
-  url = "https://www.youtube.com/watch?v=" + sys.argv[1]
+  ids = sys.argv[1].replace(' ', '').replace(';', ',').split(',')
 
 if len(sys.argv) > 2:
   resolution = sys.argv[2]
 
-if not url:
+if not ids or len(ids) == 0:
   print("USAGE:\n./video-download VIDEO_ID [RESOLUTION (default 720p)]")
 else:
-  YouTube(url).streams.get_by_resolution(resolution).download()
+  for id in ids:
+    url = "https://www.youtube.com/watch?v=" + id
+    YouTube(url).streams.get_by_resolution(resolution).download()
