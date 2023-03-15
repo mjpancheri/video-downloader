4a0469d5df38d6c78446295baeaa351ad67afd06
Marcio Pancheri <mjpancheri@gmail.com>
2023-03-15 00:24:08 -0300

    add option to print streams and download by tag

--- video-download.py
+++ video-download.py
@@ -11,9 +11,18 @@ if len(sys.argv) > 1:
 if len(sys.argv) > 2:
   resolution = sys.argv[2]
 
-if not ids or len(ids) == 0:
-  print("USAGE:\n./video-download VIDEO_ID [RESOLUTION (default 720p)]")
+if not ids or len(ids) == 0 or ids[0] == "help":
+  print("USAGE:\n./video-download VIDEO_ID [RESOLUTION (default 720p)] or")
+  print("\n./video-download VIDEO_ID print (to see all streams) or")
+  print("\n./video-download VIDEO_ID itag=NUMBER (to download specific stream)")
 else:
   for id in ids:
     url = "https://www.youtube.com/watch?v=" + id
-    YouTube(url).streams.get_by_resolution(resolution).download()
+
+    if resolution == "print":
+      print(YouTube(url).streams)
+    elif resolution.startswith("itag="):
+      itag = int(resolution.removeprefix("itag="))
+      YouTube(url).streams.get_by_itag(itag).download()
+    else:
+      YouTube(url).streams.get_by_resolution(resolution).download()
